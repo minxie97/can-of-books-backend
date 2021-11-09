5bf605006d77d7f096dc26c6f540d216b862b7c7
Minhang Xie <minxie97@gmail.com>
2021-11-08 16:19:41 -0800

    README updated and conneted to DB. Backend build done for lab 11

---
+++ seed.js
@@ -0,0 +1,38 @@
+'use strict';
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+const Book = require('./bookModel.js');
+
+async function seed() {
+    mongoose.connect(process.env.DB_URL);
+
+    await Book.create({
+        title: 'On the Road',
+        description: 'On the Road is a 1957 novel by American writer Jack Kerouac, based on the travels of Kerouac and his friends across the United States. It is considered a defining work of the postwar Beat and Counterculture generations, with its protagonists living life against a backdrop of jazz, poetry, and drug use',
+        status: 'A book',
+        email: 'minxie97@gmail.com'
+    });
+    console.log('On the Road by Jack Kerouac');
+
+    await Book.create({
+        title: 'A Clockwork Orange',
+        description: 'A Clockwork Orange is a dystopian satirical black comedy novel by English writer Anthony Burgess, published in 1962. It is set in a near-future society that has a youth subculture of extreme violence.',
+        status: 'A book',
+        email: 'minxie97@gmail.com'
+    });
+    console.log('A Clockwork Orange by Anthony Burgess');
+
+    await Book.create({
+        title: 'Kafka on the Shore',
+        description: 'Kafka on the Shore is a 2002 novel by Japanese author Haruki Murakami. The book tells the stories of the young Kafka Tamura, a bookish 15-year-old boy who runs away from his Oedipal curse, and Satoru Nakata, an old, disabled man with the uncanny ability to talk to cats. The book incorporates themes of music as a communicative conduit, metaphysics, dreams, fate, the subconscious.',
+        status: 'A book',
+        email: 'minxie97@gmail.com'
+    });
+    console.log('Kafka on the Shore by Haruki Murakami');
+
+  mongoose.disconnect();
+
+}
+
+seed()
--- server.js
+++ server.js
@@ -3,16 +3,40 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const Book = require('./bookModel.js');
 
 const app = express();
 app.use(cors());
 
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected')
+});
+
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
+app.get('/books', handleBooks)
 
-  response.send('test request received')
+async function handleBooks(req, res) {
+  let queryObj = {};
+  if (req.query.email) {
+    queryObj = {email: req.query.email}
+  }
 
-})
+  try {
+    let booksFromDB = await Book.find(queryObj);
+    if (booksFromDB) {
+      res.status(200).send(booksFromDB)
+    } else {
+      res.status(404).send('Books not found.');
+    }
+  } catch (e) {
+    res.status(500).send('Server goofed');
+  }
+}
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
